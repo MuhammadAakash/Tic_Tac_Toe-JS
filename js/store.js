9402c025848053f339e0e0056e281d64099f1cdc
MuhammadAakash <muhammadaakash249@gmail.com>
2023-05-02 12:59:27 +0500

    Refactoring the code in progress

---
+++ js/store.js
@@ -0,0 +1,26 @@
+export default class Store {
+  #state = { moves: [] };
+  constructor() {}
+
+  #getState() {
+    return this.#state;
+  }
+
+  #saveState(stateOrFn) {
+    const prevState = this.#getState();
+
+    let newState;
+
+    switch (typeof stateOrFn) {
+      case "function":
+        newState = stateOrFn();
+        break;
+      case "object":
+        newState = stateOrFn;
+        break;
+      default:
+        throw new Error("Invalid argument passed to saveState");
+    }
+    this.#state = newState;
+  }
+}
--- js/view.js
+++ js/view.js
@@ -1,17 +1,27 @@
 export default class View {
   $ = {};
+  $$ = {};
 
   constructor() {
-    this.$.menu = document.querySelector('[data-id="menu"]');
-    this.$.menuItems = document.querySelector('[data-id="menu-items"]');
-    this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
-    this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
-    this.$.squares = document.querySelectorAll('[data-id="square"]');
-    this.$.modal = document.querySelector('[data-id="modal"]');
-    this.$.modalText = document.querySelector('[data-id="modal-text"]');
-    this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
-    this.$.turn = document.querySelector('[data-id="turn"]');
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuButton = this.#qs('[data-id="menu-btn"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
+
+    // UI - only EVent listeneres
+    this.$.menuButton.addEventListener("click", (event) => {
+      this.#toggleMenu();
+    });
   }
+
+  // Event Listeners
   bindGameResetEvent(handler) {
     this.$.resetBtn.addEventListener("click", handler);
   }
@@ -21,6 +31,62 @@ export default class View {
   }
 
   bindPlayerMoveEvent(handler) {
-    this.$.squares.forEach((square) => {});
+    this.$$.squares.forEach((square) => {
+      square.addEventListener("click", handler);
+    });
+  }
+
+  // Utility Methods
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuButton.classList.toggle("border");
+
+    const icon = this.$.menuButton.querySelector("i");
+
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
+  }
+
+  setToggleIndicator(player) {
+    const icon = document.createElement("i");
+    const label = document.createElement("p");
+
+    this.$.turn.classList.add(player === 1 ? "turquoise" : "yellow");
+    this.$.turn.classList.remove(player === 1 ? "yellow" : "turquoise");
+
+    icon.classList.add("fa-solid", player === 1 ? "fa-x" : "fa-o");
+
+    label.innerText =
+      player === 1 ? "Player 1, you're up" : "Player 2, you're up";
+
+    this.$.turn.replaceChildren(icon, label);
+  }
+
+  handlePlayerMove(squareEl, player) {
+    const icon = document.createElement("i");
+
+    icon.classList.add(
+      "fa-solid",
+      player === 1 ? "fa-x" : "fa-o",
+      player === 1 ? "turquoise" : "yellow"
+    );
+
+    squareEl.replaceChildren(icon);
+  }
+
+  #qs(selector, parent) {
+    const el = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+    if (!el) throw new Error("Could not find Elements");
+
+    return el;
+  }
+
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector);
+    if (!elList) throw new Error("Could not find Elements List");
+
+    return elList;
   }
 }
