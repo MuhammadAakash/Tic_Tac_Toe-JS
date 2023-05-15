e98d0db774bb7d8fd66a79b28694d07793b9b283
Mohammad Aakash <muhammadaakash249@gmail.com>
2023-05-15 13:57:21 +0500

    Modified the code into different files.

--- js/store.js
+++ js/store.js
@@ -1,7 +1,69 @@
-export default class Store {
-  #state = { moves: [] };
-  constructor() {}
+const initialValue = {
+  moves: [],
+};
 
+export default class Store {
+  #state = initialValue;
+  constructor(players) {
+    this.players = players;
+  }
+
+  get game() {
+    const state = this.#getState();
+
+    const currentPlayer = this.players[state.moves.length % 2];
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    for (const player of this.players) {
+      const selectedPlayer = state.moves
+        .filter((move) => move.player.id === player.id)
+        .map((move) => move.squareId);
+
+      for (const pattern of winningPatterns) {
+        if (pattern.every((v) => selectedPlayer.includes(v))) {
+          winner = player;
+        }
+      }
+    }
+
+    return {
+      moves: state.moves,
+      currentPlayer,
+      status: {
+        isCompleted: winner != null || state.moves.length === 9,
+        winner,
+      },
+    };
+  }
+
+  playerMove(squareId) {
+    const state = this.#getState();
+
+    const stateCloned = structuredClone(state);
+
+    stateCloned.moves.push({
+      squareId,
+      player: this.game.currentPlayer,
+    });
+
+    this.#saveState(stateCloned);
+  }
+
+  resetGame() {
+    this.#saveState(initialValue);
+  }
   #getState() {
     return this.#state;
   }
